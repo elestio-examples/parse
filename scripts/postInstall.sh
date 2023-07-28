1d37a3accafc3bbddef69db50d297b030e547ad2
Amit Shukla <53058928+amitshuklabag@users.noreply.github.com>
2023-07-28 16:53:03 +0530

    Update postInstall.sh

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -5,3 +5,9 @@ set -o allexport; source .env; set +o allexport;
 echo "Waiting for software to be ready ..."
 sleep 30s;
 
+mkdir -p /opt/app-backups/;
+echo "docker exec -i mongo sh -c 'mongodump --port 27017 --archive' > /opt/app-backups/last.dump" > /opt/app/save.sh
+chmod +x /opt/app/save.sh;
+
+echo "docker exec -i mongo sh -c 'mongorestore --port 27017 --archive' < /opt/app-backups/last.dump" > /opt/app/restore.sh
+chmod +x /opt/app/restore.sh;
